7856cffe6ccb2a06a2c52ec77a004a1b3cc40b9a
Lukas Bernwald <lukas.bernwald@tum.de>
2021-07-15 13:54:43 +0200

    Add test for matching with splitting

--- workflows/src/integrationTest/kotlin/de/tum/best/integration/MatchingTest.kt
+++ workflows/src/integrationTest/kotlin/de/tum/best/integration/MatchingTest.kt
@@ -7,38 +7,48 @@ import de.tum.best.flows.MatchingFlow
 import de.tum.best.states.*
 import net.corda.client.rpc.CordaRPCClient
 import net.corda.core.identity.CordaX500Name
+import net.corda.core.messaging.CordaRPCOps
 import net.corda.core.messaging.startTrackedFlow
 import net.corda.core.messaging.vaultQueryBy
 import net.corda.core.utilities.getOrThrow
 import net.corda.testing.core.TestIdentity
 import net.corda.testing.core.singleIdentity
-import net.corda.testing.driver.DriverDSL
-import net.corda.testing.driver.DriverParameters
-import net.corda.testing.driver.NodeParameters
-import net.corda.testing.driver.driver
+import net.corda.testing.driver.*
 import net.corda.testing.node.TestCordapp
 import net.corda.testing.node.User
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class MatchingTest {
 
     private val identities = listOf(
         TestIdentity(CordaX500Name("PartyA", "", "GB")),
         TestIdentity(CordaX500Name("mister matching", "", "US")),
-        TestIdentity(CordaX500Name("PartyC", "", "DE"))
+        TestIdentity(CordaX500Name("PartyC", "", "DE")),
+        TestIdentity(CordaX500Name("PartyD", "", "DE"))
     )
     private val rpcUsers = listOf(
         User("userA", "password1", permissions = setOf("ALL")),
         User("userB", "password2", permissions = setOf("ALL")),
-        User("userC", "password3", permissions = setOf("ALL"))
+        User("userC", "password3", permissions = setOf("ALL")),
+        User("userD", "password4", permissions = setOf("ALL"))
     )
     private val identityUserMap = (identities zip rpcUsers).toMap()
 
 
-    @Test
-    fun `single producer and consumer listing are matched`() = withDriver {
-        // Start a pair of nodes and wait for them both to be ready.
+    private fun withDriver(test: DriverDSL.() -> Unit) = driver(
+        DriverParameters(
+            isDebug = true,
+            startNodesInProcess = true,
+            cordappsForAllNodes = listOf(
+                TestCordapp.findCordapp("de.tum.best.flows"),
+                TestCordapp.findCordapp("de.tum.best.contracts")
+            )
+        )
+    ) { test() }
+
+    private fun DriverDSL.startNodes(): Pair<List<NodeHandle>, List<CordaRPCOps>> {
         val nodeHandles = identities
             .map {
                 startNode(
@@ -58,7 +68,13 @@ class MatchingTest {
                 identityUserMap[it.first] ?: throw IllegalArgumentException("Identity ${it.first} could not be found")
             it.second.start(user.username, user.password).proxy
         }
+        return Pair(nodeHandles, proxies)
+    }
 
+    private fun initiateMarket(
+        proxies: List<CordaRPCOps>,
+        nodeHandles: List<NodeHandle>
+    ) {
         val signedMarketTimeTransaction = proxies[0].startTrackedFlow(
             InitiateMarketTimeFlow::Initiator,
             nodeHandles[1].nodeInfo.singleIdentity()
@@ -70,6 +86,12 @@ class MatchingTest {
             ::BroadcastTransactionFlow,
             signedMarketTimeTransaction
         ).returnValue.getOrThrow()
+    }
+
+    @Test
+    fun `single producer and consumer listing are matched`() = withDriver {
+        val (nodeHandles, proxies) = startNodes()
+        initiateMarket(proxies, nodeHandles)
 
         proxies[0].startTrackedFlow(
             ::ListingFlowInitiator,
@@ -97,8 +119,9 @@ class MatchingTest {
             MatchingFlow::Initiator
         ).returnValue.getOrThrow()
 
-        proxies.forEach {
-            val matchingState = it.vaultQueryBy<MatchingState>().states.single().state.data
+        proxies.slice(0..2).forEach {
+            val states = it.vaultQueryBy<MatchingState>().states
+            val matchingState = states.single().state.data
             assertEquals(nodeHandles[0].nodeInfo.singleIdentity(), matchingState.seller)
             assertEquals(nodeHandles[1].nodeInfo.singleIdentity(), matchingState.matcher)
             assertEquals(nodeHandles[2].nodeInfo.singleIdentity(), matchingState.buyer)
@@ -107,15 +130,59 @@ class MatchingTest {
         }
     }
 
-    private fun withDriver(test: DriverDSL.() -> Unit) = driver(
-        DriverParameters(
-            isDebug = true,
-            startNodesInProcess = true,
-            cordappsForAllNodes = listOf(
-                TestCordapp.findCordapp("de.tum.best.flows"),
-                TestCordapp.findCordapp("de.tum.best.contracts")
-            )
+    @Test
+    fun `producer listing must be split`() = withDriver {
+        val (nodeHandles, proxies) = startNodes()
+        initiateMarket(proxies, nodeHandles)
+
+        proxies[0].startTrackedFlow(
+            ::ListingFlowInitiator,
+            ElectricityType.Renewable,
+            5,
+            6,
+            nodeHandles[1].nodeInfo.singleIdentity(),
+            ListingType.ProducerListing
+        ).returnValue.getOrThrow()
+
+        assertEquals(5, proxies[0].vaultQueryBy<ListingState>().states.first().state.data.unitPrice)
+
+        proxies[2].startTrackedFlow(
+            ::ListingFlowInitiator,
+            ElectricityType.Renewable,
+            7,
+            3,
+            nodeHandles[1].nodeInfo.singleIdentity(),
+            ListingType.ConsumerListing
+        ).returnValue.getOrThrow()
+
+        assertEquals(7, proxies[1].vaultQueryBy<ListingState>().states.last().state.data.unitPrice)
+
+        proxies[1].startTrackedFlow(
+            MatchingFlow::Initiator
+        ).returnValue.getOrThrow()
+
+        val states = proxies[0].vaultQueryBy<MatchingState>().states
+        val (firstMatchingState, secondMatchingState) = states.first().state.data to states.last().state.data
+
+        assertTrue(
+            (firstMatchingState.buyer == nodeHandles[1].nodeInfo.singleIdentity()
+                    && secondMatchingState.buyer == nodeHandles[2].nodeInfo.singleIdentity()) ||
+                    (firstMatchingState.buyer == nodeHandles[2].nodeInfo.singleIdentity()
+                    && secondMatchingState.buyer == nodeHandles[1].nodeInfo.singleIdentity())
         )
-    ) { test() }
+
+        assertTrue(
+            firstMatchingState.unitPrice == 5
+                    && secondMatchingState.unitPrice == 3 ||
+                    firstMatchingState.unitPrice == 3
+                    && secondMatchingState.unitPrice == 5
+        )
+
+        setOf(firstMatchingState, secondMatchingState).forEach {
+            assertEquals(nodeHandles[0].nodeInfo.singleIdentity(), it.seller)
+            assertEquals(nodeHandles[1].nodeInfo.singleIdentity(), it.matcher)
+            assertEquals(3, it.unitAmount)
+        }
+    }
 
 }
